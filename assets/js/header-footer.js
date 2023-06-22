fcbc437a706f99e4347b3f28184d05165bd124bb
Tanisha Gupta <tanishagupta27200@gmail.com>
2023-06-22 15:55:39 +0530

    home-page

--- assets/js/header-footer.js
+++ assets/js/header-footer.js
@@ -2,3 +2,17 @@ $(function(){
     $("#header").load("../assets/html-modules/header.html");
     $("#footer").load("../assets/html-modules/footer.html");
 })
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("stickynav").style.top = "0";
+    
+//   } else {
+//     document.getElementById("stickynav").style.top = "-100%";
+   
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
